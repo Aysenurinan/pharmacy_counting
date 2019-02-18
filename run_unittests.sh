f8b84d7c083e225a8865f43cf5ce44db1ad736cd
Aysenurinan <aysenur.corlu@gmail.com>
2019-02-18 14:00:34 -0800

    Delete run_unittests.sh

--- run_unittests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
-#
-
-echo "[$(date)]" &>> unit_tests/results.txt
-python3 -m unittest discover -v &>> unit_tests/results.txt
-
-read -p "Press enter to continue"
